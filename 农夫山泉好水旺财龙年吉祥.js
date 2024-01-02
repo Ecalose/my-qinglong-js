be6cbccd5da0d99e3dc4083ae0511974b1b8c9f4
To be rich <33859558+Huansheng1@users.noreply.github.com>
2024-01-02 12:02:44 +0800

    Create 农夫山泉好水旺财龙年吉祥.js

---
+++ 农夫山泉好水旺财龙年吉祥.js
@@ -0,0 +1,21 @@
+/**
+ * 农夫山泉好水旺财龙年吉祥 V1.00
+ * const $ = new Env("农夫山泉好水旺财龙年吉祥");
+ * cron 10 6,15 * * * 农夫山泉好水旺财龙年吉祥.js
+ * 
+ * ========= 青龙--配置文件 ===========
+ * # 项目名称
+ * export nssqln='apitoken#unique_identity#抽奖完整提交数据'
+
+ * 搜索 /api/v1/marketinglottery 自己复制协议头上的apitoken、unique_identity 以及 完整提交body数据，用 # 符号连接
+
+ * 多账号换行或&隔开
+
+ * 奖励：每天自动抽三回
+ *
+ * tg反馈群： https://t.me/huan_sheng
+ * 
+ * 脚本更新地址：https://github.com/Huansheng1/my-qinglong-js 
+ * ====================================
+ *   
+ */
